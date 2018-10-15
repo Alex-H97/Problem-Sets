8e5d336faeb350f7c94706249d2583c235951746
Alex-H97 <Alex-H97@users.noreply.github.com>
2018-10-15 13:29:16 +0000

    20181015T132856Z

---
+++ hello.py
@@ -0,0 +1,12 @@
+# Define main function
+def main():
+
+    # Print "hello, world"
+    print("hello, world")
+
+
+# Check if ran as main
+if __name__ == "__main__":
+
+    # Run main function
+    main()
